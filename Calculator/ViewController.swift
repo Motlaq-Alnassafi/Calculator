aaa97838db8b4d8fb54bd5995ac9247cb01a4b4d
Motlaq <motlaqalnesafi@gmail.com>
2023-09-26 13:12:32 +0300

    made the button functional

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func Button(_ sender: Any) {
+        titleLabel.text = "HELLO"
+    }
+    
 }
 
