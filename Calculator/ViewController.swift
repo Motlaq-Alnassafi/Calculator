3c33f75e5dacb9cd0814b69b67b579d0cb1a662e
Motlaq <motlaqalnesafi@gmail.com>
2023-09-26 12:40:03 +0300

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Eng. Motlaq Alnassafi on 26/09/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
