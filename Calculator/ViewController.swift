ea1fa50eccc191e64f5d1fa358a5b3f413b62312
Motlaq <motlaqalnesafi@gmail.com>
2023-09-27 12:23:51 +0300

    Changed the R to a C

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -26,6 +26,7 @@ class ViewController: UIViewController {
         labelTextOne += "0"
         textLabel.text = labelTextOne
     }
+    
     @IBAction func button1(_ sender: UIButton) {
         labelTextOne += "1"
         textLabel.text = labelTextOne
